2d4eeddee8d7dfe6c218e0ff5cb5d68a931ecd58
OneFourteen Studio <onefourteenstudio114@gmail.com>
2023-08-09 09:08:57 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const userPfp = document.getElementById('user-pfp')
+const name = document.getElementById('name')
+const location = document.getElementById('location')
+const
+
+
+
+
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/Vincent van Gogh.png",
+        post: "images/vvg.png",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/Gustave Courbet.png",
+        post: "images/gc.png",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/Joseph Ducreux.png",
+        post: "images/jd.png",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+
+function renderPosts () {
+    for(let i = 0; i< posts.length; i++) {
+
+    }
+}
